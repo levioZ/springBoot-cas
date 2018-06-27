586767c09796f533dbb86ad26525ea10fe60704e
eli <kuruxue@qq.com>
2018-06-27 10:23:20 +0800

    update mian

--- src/main/java/com/eli/cas/CasApplication.java
+++ src/main/java/com/eli/cas/CasApplication.java
@@ -1,12 +1,13 @@
 package com.eli.cas;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class CasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CasApplication.class, args);
+		run(CasApplication.class, args);
 	}
 }
